91579cba77de2e9bafc707d9c2cbdfc9cd20bb86
EvanStJohn <46731712+EvanStJohn@users.noreply.github.com>
2019-10-08 10:53:05 -0700

    change

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 #include <ctime>
 #include <fstream>
 #include <vector>
+#include <cstdlib>
 using namespace std;
 
 class SuperMarket{
 private:
     vector<int> item;
     vector<int> amount;
+    vector<string> menuArray;
 public:
     void greeting();
     void menu();
@@ -42,9 +44,12 @@ void SuperMarket::greeting()
     }
 }
 
+
 void SuperMarket::menu()
 {
-    string menu = read_datafile();
+    string menu = read_datafile(), tempPrice, line;
+    double price = 0;
+    size_t prev = 0, pos = 0;
     cout << menu << endl;
     cout << "What items do you want to buy (1-5)?" <<endl;
 
@@ -58,6 +63,12 @@ void SuperMarket::menu()
         cout << "how many of this item do you want to buy" << endl;
         cin >> input;
         amount.push_back(input);
+        tempPrice = menuArray.at(input-1);
+        prev = tempPrice.length()-1;
+        line = tempPrice;
+        pos = line.find_last_of(' ', prev); //or find_last_of( " ';"), pos
+        tempPrice = line.substr(pos+1, 4);
+        price += atof("tempPrice");
 
         string input2;
         cout << "would you like to buy another item (y/n)" << endl;
@@ -68,13 +79,19 @@ void SuperMarket::menu()
             cout << "What items do you want to buy (1-5)?" <<endl;
     }
 
-    //extract price from back of the line by figuring out first empty space
-
+    cout << "You spent a total of $" << price << endl;
+    //extract the menu prices
+    //This is how to get the last word in a string
+    //lines need to take in a single line of the text
+//    pos = line.find_last_of(" ';", prev); //or find_last_of( ' ' ), pos
+//    tempPrice = line.substr(prev, pos-prev);
+    //make into double and then add the full price
+    //price += atof("tempPrice");
 }
 
 void SuperMarket::bill(int bill)
 {
-
+    //keep track of what the user buys through an array?
 }
 
 void SuperMarket::printInvoice()
@@ -82,6 +99,7 @@ void SuperMarket::printInvoice()
 
 }
 
+//reads the menu file
 string SuperMarket::read_datafile()
 {
     string menuList;
@@ -93,6 +111,7 @@ string SuperMarket::read_datafile()
     string tempStr;
     while(std::getline(in_stream,tempStr)){
         menuList += tempStr +="\n";
+        menuArray.push_back(tempStr);
     }
 
     in_stream.close();
@@ -101,7 +120,7 @@ string SuperMarket::read_datafile()
 
 void SuperMarket::print_receipt()
 {
-
+    //I'm thinking of passing the int price from above here for the total
 }
 
 int main ()
@@ -109,7 +128,5 @@ int main ()
     SuperMarket test;
     test.greeting();
     test.menu();
-
-
     return 0;
 }
